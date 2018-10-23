283983ab82a5aea783a1f559fd69af9925360c18
Basu Dubey <basu.96@gmail.com>
2018-10-23 05:34:51 +0530

    Add template to view all students

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 from flask import *
 
 from utils import Student
+from db import DBHandler
 
 app = Flask(__name__)
+DBNAME = 'db.sqlite3'
 
 @app.route('/')
 def home():
     return render_template('base.html')
+
+@app.route('/view')
+def view_all():
+    db = DBHandler(DBNAME)
+    students = db.get_all_students()
+    # TODO: merge_sort(student_list)
+    return render_template('view_all.html', student_list = students)
